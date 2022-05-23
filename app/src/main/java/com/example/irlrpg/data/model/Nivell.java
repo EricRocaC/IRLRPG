f9b8889b87b45870a5d3df1862dff21f7558c72f
Eric <rocacepero98@gmail.com>
2022-05-23 15:57:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/irlrpg/data/model/Nivell.java
@@ -0,0 +1,13 @@
+package com.example.irlrpg.data.model;
+
+public class Nivell {
+    public int currentXP = 0;
+    public int lastLevel = 0;
+    public int nextLevel = 0;
+    int xpBetweenLevels = nextLevel - lastLevel;
+    int xpSinceLevelUp = currentXP - lastLevel;
+    float percentageOfXP = xpSinceLevelUp / (float) xpBetweenLevels;
+    //int barWidth = (int) (percentageOfXP * maxWidth);
+
+
+}
